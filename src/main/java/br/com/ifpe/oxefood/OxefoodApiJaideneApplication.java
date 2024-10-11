72470fc24bb1c4ae5490cb5f6ff84840f0a8cc07
aluno <aluno@ifpe>
2024-10-10 21:24:24 -0300

    incuindo o spring inicitializing

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiJaideneApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiJaideneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiJaideneApplication.class, args);
+	}
+
+}
